d56bb0eafeb58b20308a5fd96db6a6f29954b741
Weston Ruter <weston@x-team.com>
2013-08-07 23:24:36 -0700

    Initial release

---
+++ missing-upload-placeholders.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Plugin Name: Missing Upload Placeholders
+ * Plugin URI:  http://github.com/x-team/wp-missing-upload-placeholders
+ * Description: Redirect requests to non-existent uploaded images to a placeholder service like placehold.it or placekitten.com. For use during development.
+ * Version:     0.9
+ * Author:      X-Team
+ * Author URI:  http://x-team.com/
+ * License:     GPLv2+
+ */
+
+/**
+ * Copyright (c) 2013 X-Team (http://x-team.com/)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+class Missing_Upload_Placeholders {
+	static function setup() {
+		add_action( 'template_redirect', array( __CLASS__, 'handle_missing_upload' ) );
+	}
+
+	/**
+	 * @action template_redirect
+	 */
+	static function handle_missing_upload() {
+		global $wpdb;
+		if ( ! is_404() ) {
+			return;
+		}
+
+		$upload_dir = wp_upload_dir();
+		$base_url_path = parse_url( $upload_dir['baseurl'], PHP_URL_PATH );
+
+		if ( strpos( $_SERVER['REQUEST_URI'], $base_url_path ) !== 0 ) {
+			return;
+		}
+		$relative_upload_path = substr( $_SERVER['REQUEST_URI'], strlen( $base_url_path ) + 1 );
+		$relative_upload_path = parse_url( $relative_upload_path, PHP_URL_PATH );
+
+		$attached_file = $relative_upload_path;
+		$width = null;
+		$height = null;
+		if ( preg_match( '#(.+)(?:-(\d+)x(\d+))(\.\w+)#', $relative_upload_path, $matches ) ) {
+			$attached_file = $matches[1] . $matches[4];
+			$width = (int) $matches[2];
+			$height = (int) $matches[3];
+		}
+
+		$query = $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_wp_attached_file' AND meta_value = %s ", $attached_file );
+		$attachment_id = $wpdb->get_var( $query );
+		if ( empty( $attachment_id ) ) {
+			return;
+		}
+		if ( ! wp_attachment_is_image( $attachment_id ) ) {
+			return;
+		}
+
+		/**
+		 * Optionally redirect missing image requests to another server
+		 * Requires specifying a MISSING_UPLOADED_IMAGE_REDIRECT_SERVER constant,
+		 * and/or the addition of missing_uploaded_image_redirect_server filter to return
+		 * the domain of the server for which the attachment image is located on.
+		 */
+		$default_server = null;
+		if ( defined( 'MISSING_UPLOADED_IMAGE_REDIRECT_SERVER' ) ) {
+			$default_server = MISSING_UPLOADED_IMAGE_REDIRECT_SERVER;
+		}
+		$redirect_server = apply_filters( 'missing_uploaded_image_redirect_server', $default_server, $attachment_id );
+		if ( $redirect_server ) {
+			$url = is_ssl() ? 'https://' : 'http://';
+			$url .= $redirect_server;
+			$url .= $_SERVER['REQUEST_URI'];
+			wp_redirect( $url );
+			exit;
+		}
+
+		// If no dimensions were requested, use original dimensions stored when image was uploaded
+		$metadata = wp_get_attachment_metadata( $attachment_id );
+		if ( empty( $width ) && ! empty( $metadata['width'] ) ) {
+			$width = $metadata['width'];
+		}
+		if ( empty( $height ) && ! empty( $metadata['height'] ) ) {
+			$height = $metadata['height'];
+		}
+
+		// If no image dimensions could be found, use the large image size dimensions
+		if ( empty( $width ) ) {
+			$width = get_option( 'large_size_w' );
+		}
+		if ( empty( $height ) ) {
+			$height = get_option( 'large_size_h' );
+		}
+
+		$filter_args = compact( 'attached_file', 'width', 'height', 'attachment_id' );
+
+		$default_builtin = defined( 'MISSING_UPLOADED_IMAGE_PLACEHOLDER_BUILTIN' )
+			? MISSING_UPLOADED_IMAGE_PLACEHOLDER_BUILTIN
+			: 'placeholdit';
+
+		$default_filter = array( __CLASS__, sprintf( 'filter_%s_image_url', $default_builtin ) );
+		if ( is_callable( $default_filter ) ) {
+			$url = call_user_func( $default_filter, null, $filter_args );
+		}
+		else {
+			trigger_error( sprintf( 'Uncallable handler %s for missing image upload fallback', json_encode( $handler ) ), E_USER_WARNING );
+		}
+		$url = apply_filters( 'missing_uploaded_image_placeholder', $url, $filter_args );
+
+		wp_redirect( $url );
+		exit;
+	}
+
+	/**
+	 * Get URL to a placeholder image on placehold.it
+	 * @param string|null $url
+	 * @param array|string $args {attached_file, width, height, attachment_id}
+	 * @return string
+	 */
+	static function filter_placeholdit_image_url( $url, $args = '' ) {
+		extract( wp_parse_args( $args ) ); // => $attached_file, $width, $height, $attachment_id
+		$name_hash = md5( $attached_file );
+		$bgcolor = substr( $name_hash, 0, 6 );
+		$fgcolor = substr( $name_hash, 6, 6 );
+		$ext = preg_replace( '/.+\./', '', $attached_file );
+		$name = rtrim( basename( $attached_file, $ext ), '.' );
+		$ext = 'png'; // yeah, JPEGs look awful
+		$name .= " ({$width}x{$height})";
+		$name = urlencode( $name );
+		$url = "http://placehold.it/{$width}x{$height}/{$bgcolor}/{$fgcolor}/{$ext}&text={$name}";
+		return $url;
+	}
+
+	/**
+	 * Get URL to a placeholder color image on placekitten.com
+	 * @param string|null $url
+	 * @param array|string $args {attached_file, width, height, attachment_id}
+	 * @return string
+	 */
+	static function filter_placekitten_color_image_url( $url, $args ) {
+		extract( wp_parse_args( $args ) ); // => $attached_file, $width, $height, $attachment_id
+		$url = "http://placekitten.com/$width/$height";
+		return $url;
+	}
+
+	/**
+	 * Get URL to a placeholder grayscale image on placekitten.com
+	 * @param string|null $url
+	 * @param array|string $args {attached_file, width, height, attachment_id}
+	 * @return string
+	 */
+	static function filter_placekitten_grayscale_image_url( $url, $args ) {
+		extract( wp_parse_args( $args ) ); // => $attached_file, $width, $height, $attachment_id
+		$url = "http://placekitten.com/g/$width/$height";
+		return $url;
+	}
+}
+
+add_action( 'plugins_loaded', array( 'Missing_Upload_Placeholders', 'setup' ) );
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Missing Upload Placeholders ===
+Contributors:      X-team, westonruter
+Tags:              placeholders, uploads, development, images, 404
+Requires at least: 3.5
+Tested up to:      3.6
+Stable tag:        trunk
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Redirect requests to non-existent uploaded images to a placeholder service like placehold.it or placekitten.com. For use during development.
+
+== Description ==
+
+Activate this plugin to redirect all requests for missing uploaded images on your blog to your favorite placeholder image service, such as [placehold.it](https://placehold.it) or [placekitten.com](http://placekitten.com/). Note that although kittens are cute, the placehold.it service is actually more useful because the background and foreground color can remain consistant across all image sizes (e.g. full size vs thumbnail in a gallery), and so it is easier to see which images in a page are related to each other. (You can change the default placehold.it service to placekitten.com by defining `MISSING_UPLOADED_IMAGE_PLACEHOLDER_BUILTIN` to be `placekitten_color` or `placekitten_grayscale`, or supplying those same values via the `missing_uploaded_image_placeholder_builtin` filter).
+
+**This plugin is for use during development only.** It is expected that this plugin will be activated on your local development environment (e.g. on Vagrant or XAMPP), or on your staging server. This plugin is especially useful when working on a team where you share around a database dump but not the uploaded images (which should always be omitted from the code repository), so if you give a database dump to another developer but don't include the uploaded images, with this plugin enabled they will see a placeholder where the uploaded image appears. This plugin is an alternative approach to what is offered by the [Uploads by Proxy](http://wordpress.org/plugins/uploads-by-proxy/) plugin.
+
+If you have applied the production database to another environment which lacks the uploaded files, but you know that all images referenced in the database do exist on production, you can define the `MISSING_UPLOADED_IMAGE_REDIRECT_SERVER` constant or filter `missing_uploaded_image_redirect_server` to short-circuit the placeholder service and redirect the image request to that server.
+
+This plugin will not work if you are on a multisite network that uses the old system for referring to uploaded files, where the URL includes `/files/` which is intercepted by a rewrite rule and passed directly to `ms-files.php`. See [#19235](http://core.trac.wordpress.org/ticket/19235 "Turn ms-files.php off by default"). Similarly, make sure that missing uploaded files get served by the WordPress 404 handler, not Apache/Nginx. If you are using Nginx with the default Varying Vagrant Vagrants config, you'll want to remove `png|jpg|jpeg|gif` from the following location rule in `nginx-wp-common.conf` (or remove it altogether):
+
+	# Handle all static assets by serving the file directly. Add directives 
+	# to send expires headers and turn off 404 error logging.
+	location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+		expires 24h;
+		log_not_found off;
+	}
+
+You can add support for your own favorite placeholder services by filtering `missing_uploaded_image_placeholder`.
+For example, you can add this to your `functions.php` or drop it into a `mu-plugin`:
+
+	<?php
+	/**
+	 * Use Flickholdr as placeholder service
+	 * @param null|string $url
+	 * @param array $args  {attached_file, width, height, attachment_id}
+	 */
+	function my_filter_missing_uploaded_image_placeholder( $url, $args ) {
+		$attachment = get_post( $args['attachment_id'] );
+		$tags = join( ' ', array(
+			$attachment->post_title,
+			$attachment->post_excerpt,
+			$attachment->post_content,
+			$attachment->_wp_attachment_image_alt
+		) );
+		$tags = strtolower( preg_replace( '#[^A-Za-z0-9]+#', ',', $tags ) );
+		$tags = trim( $tags, ',' );
+		$url = sprintf( 'http://flickholdr.com/%d/%d/%s', $args['width'], $args['height'], $tags );
+		return $url;
+	}
+	add_filter( 'missing_uploaded_image_placeholder', 'my_filter_missing_uploaded_image_placeholder', 10, 2 );
+
+== Changelog ==
+
+= 1.0 =
+First Release
