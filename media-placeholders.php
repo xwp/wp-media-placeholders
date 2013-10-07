bf616849bed053bf085d17da66f23c069413ee47
Weston Ruter <weston@x-team.com>
2013-10-06 23:47:37 -0400

    Update Author URI

--- media-placeholders.php
+++ media-placeholders.php
@@ -5,7 +5,7 @@
  * Description: Redirect requests to non-existent uploaded images to a placeholder service like placehold.it or placekitten.com. For use during development.
  * Version:     0.9.1
  * Author:      X-Team
- * Author URI:  http://x-team.com/
+ * Author URI:  http://x-team.com/wordpress/
  * License:     GPLv2+
  */
 
